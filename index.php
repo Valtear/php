c7145027e60100cf00bb15afa207ec72e08c6e1b
Valtear <38602736+Valtear@users.noreply.github.com>
2024-03-02 20:30:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,60 @@
+<?php
+session_start();
+if(!isset($_SESSION["valueArray"])){
+    for ($i = 0;$i<9;$i++){
+        $_SESSION["valueArray"][$i] = 0;
+    }
+}
+if(!empty($_POST['myRadio'])){
+    $value = $_POST['myRadio'] - 1;
+    $_SESSION['valueArray'][$value] = 1;
+
+    do{
+        if($_SESSION['valueArray'][$rand = rand(0,9)] != 1){
+            $_SESSION['valueArray'][$rand] = 2;
+        }
+        break;
+    }
+    while(true);
+
+}
+?>
 <!Doctype html>
 <html>
     <head>       
     </head>
     <body>
-        <div>
-            <?php
-                session_start();
-                $id = 0;
-
+        <?php           
+            function myTable(){
                 echo "<table>";
                 for($i = 0; $i < 3; $i++){
                     echo "<tr>";
                     for($j = 0; $j < 3; $j++){
-                        echo "<td style = 'border: 2px solid black'>"
-                        //$_SESSION['radioArray'][] = "<input type = 'radio' id = '" . $id++ . "'>";
-                        $radioArray[] = "<input type = 'radio' id = '" . $id++ . "'>";
-                        "</td>";
+                        echo "<td style = 'border: 2px solid black'>";
+                        if($_SESSION['valueArray'][($i*3)+$j] === 1){
+                            echo "X";
+                        }
+                        elseif($_SESSION['valueArray'][($i*3)+$j] === 2){
+                            echo "O";
+                        }
+                        else{
+                            echo "<input type = 'radio' name = 'myRadio' value = '" . (($i*3)+$j) + 1 . "'>";
+                        }                       
+                        echo "</td>";
                     }
                     echo "</tr>";
                 }
                 echo "</table>";  
-                         
-            ?>
-        </div>       
+            }                            
+        ?>    
         <div>
-            <form action = '' method='POST'>  
+            <form action = 'index.php' method='POST'>  
+                <?php  
+                    myTable(); 
+                ?>   
                 <input type="submit" value="Отправить">
             </form>
-            <?php
-                //echo $_SESSION['radioArray'][1];
-                echo $radioArray[1];
-            ?>
+            <a href="reset.php">сброс</a>
         </div>
     </body>
 </html>
