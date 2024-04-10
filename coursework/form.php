0f4d49fd70deedeb5db353fab5b3c6cacf02b654
Valtear <38602736+Valtear@users.noreply.github.com>
2024-04-10 22:32:51 +0300

    Update form.php

--- coursework/form.php
+++ coursework/form.php
@@ -5,9 +5,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   </head>
   <body>
-       <form action="#" method="POST">
+       <form action="index.php" method="POST">
             <div class="container">
-                <div class="d-flex justify-content-center">
+                <div class="d-flex justify-content-center p-5">
                     <h4>Карта клиента<h4>
                 </div>
                 <div class="input-group mb-3">
@@ -24,13 +24,14 @@
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1">№ счета</span>
-                    <input type="text" class="form-control" paria-label="Имя пользователя" aria-describedby="basic-addon1">
+                    <input type="text" class="form-control" paria-label="Имя пользователя" aria-describedby="basic-addon1" readonly>
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1">Баланс</span>
                     <input type="text" class="form-control"  aria-label="Имя пользователя" aria-describedby="basic-addon1">
                 </div>
-                <div class="d-flex justify-content-end">
+
+                <div class="d-flex justify-content-end p-5">
                     <button class="btn btn-primary" type="submit">Сохранить</button>
                 </div>
             </div>
